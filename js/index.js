9ab8a7b3a7b031ef3e0aa4d7dd926880b1902f06
paolaGAR1 <78180166+paolaGAR1@users.noreply.github.com>
2021-09-22 20:34:56 -0500

    AHORA SI

---
+++ js/index.js
@@ -0,0 +1,2 @@
+//ReactDOM.render(<h1>Render</h1>, document.querySelector("#root"));
+ReactDOM.render(<Contador />, document.querySelector("#root"));
---
+++ js/register.js
@@ -0,0 +1,6 @@
+if("serviceWorker" in navigator){
+    //Registramos el service worker
+    navigator.serviceWorker.register("./sw.js")
+}
+
+
